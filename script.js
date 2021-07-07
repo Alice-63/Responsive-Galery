eed33b6f1aaa5ac27d4802158d5c669f132080d8
Alice-63 <alicetindag84@gmail.com>
2021-07-07 11:47:59 +0200

    add

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const bilder=document.querySelectorAll("#bilder>img");
+const kreuz=document.querySelector("#lightbox i")
 
 
 bilder.forEach(test)
@@ -6,10 +7,27 @@ bilder.forEach(test)
 
 function test(e)
 {
-    e.onclick=function () { document.getElementById("lightbox").style.display="flex"}
+    e.onclick=function () 
+    { 
+        document.getElementById("lightbox").style.display="flex"
+
+        const quelle=e.getAttribute("src");
+        const ligthImg=document.querySelector("#lightbox img");
+
+
+        ligthImg.setAttribute("src", quelle);
+
+
+
+    
+    
+    }
 }
 
-
+kreuz.onclick=function()
+{
+    document.getElementById("lightbox").style.display="none"
+}
 
 
 
